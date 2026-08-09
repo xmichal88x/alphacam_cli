931855528a57c7f251e50f4ed0be6cde57831bd1
root <root@opencode.tailb25641.ts.net>
2026-08-09 11:35:59 +0000

    Fix: vdb5_set_job_material.ps1 param() must be first statement (PS 5.1)

--- scripts/vdb5_set_job_material.ps1
+++ scripts/vdb5_set_job_material.ps1
@@ -1,9 +1,9 @@
-$ErrorActionPreference = 'Stop'
-[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 param(
     [string]$JobName,
     [int]$MaterialID
 )
+$ErrorActionPreference = 'Stop'
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 Add-Type -Path 'C:\Program Files\Hexagon\ALPHACAM 2025\VistaDB.5.NET40.dll'
 $conn = New-Object VistaDB.Provider.VistaDBConnection('Data Source=C:\ALPHACAM\LICOMDAT\Automation Manager Data\AutomationManager.vdb5')
 $conn.Open()
